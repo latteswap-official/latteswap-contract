a6c489398d09e91109235d64adef7363f2abc558
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-08-25 17:29:18 +0700

    fix: when nftaddress is 0, do not proceed

--- contracts/nft/Booster.sol
+++ contracts/nft/Booster.sol
@@ -346,6 +346,9 @@ contract Booster is
   ) external override inExec {
     NFTStakingInfo memory stakingNFT = userStakingNFT[stakeToken][userAddr];
     UserInfo storage user = userInfo[stakeToken][userAddr];
+    if (stakingNFT.nftAddress == address(0)) {
+      return;
+    }
     (, uint256 currentEnergy, uint256 boostBps) = boosterConfig.energyInfo(
       stakingNFT.nftAddress,
       stakingNFT.nftTokenId
