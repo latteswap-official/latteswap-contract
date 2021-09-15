894d60e07317ff21acf9139de483db60dc5a376f
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-09-15 16:26:24 +0700

    refactor: change target folder for timelock result to scripts/ instead of deploy/

--- utils/file.ts
+++ utils/file.ts
@@ -2,5 +2,5 @@ import * as fs from "fs";
 
 export function write(fileName: string, content: any) {
   const timestamp = Math.floor(Date.now() / 1000);
-  fs.writeFileSync(`./deploy/results/${timestamp}_${fileName}.json`, JSON.stringify(content, null, 2));
+  fs.writeFileSync(`./scripts/results/${timestamp}_${fileName}.json`, JSON.stringify(content, null, 2));
 }
