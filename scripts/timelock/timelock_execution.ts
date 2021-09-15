c7c9e9d1f4a23dd02671db57d01b6bd7331ae4d3
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-09-15 16:36:55 +0700

    feat: create a script for get and executing timelocked tx from a timelock file

---
+++ scripts/timelock/timelock_execution.ts
@@ -0,0 +1,35 @@
+import { ITimelockResponse, withNetworkFile } from "../../utils";
+import TimelockTransactions from "../results/mock.json";
+import { FileService, TimelockService } from "../../utils";
+
+async function main() {
+  const timelockTransactions: Array<ITimelockResponse> = [];
+  try {
+    for (const timelockTransaction of TimelockTransactions) {
+      timelockTransactions.push(
+        await TimelockService.executeTransaction(
+          timelockTransaction.info,
+          timelockTransaction.queuedAt,
+          timelockTransaction.executionTransaction,
+          timelockTransaction.target,
+          timelockTransaction.value,
+          timelockTransaction.signature,
+          timelockTransaction.paramTypes,
+          timelockTransaction.params,
+          timelockTransaction.eta
+        )
+      );
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  FileService.write("timelock-execution", timelockTransactions);
+}
+
+withNetworkFile(main)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
