f86622401d6a9f4579ec5b59411b222983656123
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-09-19 16:09:00 +0700

    feat: add a script for setting a new delay for timelock

---
+++ scripts/timelock/timelock/change_delay.ts
@@ -0,0 +1,46 @@
+import { getConfig, withNetworkFile, FileService, TimelockService, ITimelockResponse } from "../../../utils";
+
+interface IStakingPool {
+  STAKING_TOKEN_ADDRESS: string;
+}
+
+type IStakingPools = Array<IStakingPool>;
+
+async function main() {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  const config = getConfig();
+  const timelockTransactions: Array<ITimelockResponse> = [];
+  const DELAY = 86400; // 24 hrs
+  const TIMELOCK_ETA = "";
+
+  console.log(`>> Queue Master Transaction to setDelay in timelock ${config.Timelock} `);
+  timelockTransactions.push(
+    await TimelockService.queueTransaction(
+      `setDelay in Timelock ${config.Timelock} from 6 hrs to ${DELAY / 60 / 60}`,
+      config.Timelock,
+      "0",
+      "setDelay(uint256)",
+      ["uint256"],
+      [DELAY],
+      TIMELOCK_ETA
+    )
+  );
+  console.log("✅ Done");
+
+  await FileService.write("set-timelock-delay", timelockTransactions);
+}
+
+withNetworkFile(main)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
