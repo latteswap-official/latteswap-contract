fe6324abe6ac5e3dc1e2a8f067e8af146479d36d
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-09-23 14:01:55 +0700

    feat: add Harvest event in MasterBarista

---
+++ scripts/farm/timelock/timelock_upgrade_master_barista.ts
@@ -0,0 +1,53 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers, upgrades, network } from "hardhat";
+import { MasterBarista__factory, Timelock__factory } from "../../../typechain";
+import { getConfig, withNetworkFile } from "../../../utils";
+
+async function main() {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  const config = getConfig();
+  const TARGETED_MASTER_BARISTA = config.MasterBarista;
+  const EXACT_ETA = "";
+
+  const timelock = Timelock__factory.connect(config.Timelock, (await ethers.getSigners())[0]);
+
+  console.log(`============`);
+  console.log(`>> Upgrading MasterBarista through Timelock + ProxyAdmin`);
+  console.log(">> Prepare upgrade & deploy if needed a new IMPL automatically.");
+  const MasterBarista = (await ethers.getContractFactory("MasterBarista")) as MasterBarista__factory;
+  const preparedMasterBarista = await upgrades.prepareUpgrade(TARGETED_MASTER_BARISTA, MasterBarista);
+  console.log(`>> Implementation address: ${preparedMasterBarista}`);
+  console.log("✅ Done");
+
+  console.log(`>> Queue tx on Timelock to upgrade the implementation`);
+  await timelock.queueTransaction(
+    config.ProxyAdmin,
+    "0",
+    "upgrade(address,address)",
+    ethers.utils.defaultAbiCoder.encode(["address", "address"], [TARGETED_MASTER_BARISTA, preparedMasterBarista]),
+    EXACT_ETA
+  );
+  console.log("✅ Done");
+
+  console.log(`>> Generate executeTransaction:`);
+  console.log(
+    `await timelock.executeTransaction('${config.ProxyAdmin}', '0', 'upgrade(address,address)', ethers.utils.defaultAbiCoder.encode(['address','address'], ['${TARGETED_MASTER_BARISTA}','${preparedMasterBarista}']), ${EXACT_ETA})`
+  );
+  console.log("✅ Done");
+}
+
+withNetworkFile(main)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
