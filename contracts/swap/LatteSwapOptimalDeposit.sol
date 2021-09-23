5bc992c3517cd7b4c1812c7bc12fd552e5c365d6
Earl Gray Frappuccino <elgrayfrappuccino.kub@gmail.com>
2021-09-23 22:43:34 +0700

    feat: add support for native token optimal deposit

--- contracts/swap/LatteSwapOptimalDeposit.sol
+++ contracts/swap/LatteSwapOptimalDeposit.sol
@@ -9,9 +9,9 @@ import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol
 import "./interfaces/ILatteSwapFactory.sol";
 import "./interfaces/ILatteSwapPair.sol";
 import "./interfaces/ILatteSwapRouter.sol";
+import './interfaces/IWBNB.sol';
 
 import "./libraries/LatteSwapMath.sol";
-import './libraries/LatteSwapLibrary.sol';
 import "./libraries/SafeToken.sol";
 
 contract LatteSwapOptimalDeposit is ReentrancyGuardUpgradeable {
@@ -20,14 +20,16 @@ contract LatteSwapOptimalDeposit is ReentrancyGuardUpgradeable {
 
   ILatteSwapFactory public factory;
   ILatteSwapRouter public router;
+  address public WBNB;
 
   /// @dev Create a new add two-side optimal strategy instance.
   /// @param _router The Router smart contract.
-  function initialize(ILatteSwapRouter _router) external initializer {
+  function initialize(ILatteSwapRouter _router, address _WBNB) external initializer {
     ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
 
     factory = ILatteSwapFactory(_router.factory());
     router = _router;
+    WBNB = _WBNB;
   }
 
   /// @dev Compute swap amount to optimal deposit
@@ -69,7 +71,7 @@ contract LatteSwapOptimalDeposit is ReentrancyGuardUpgradeable {
     uint256 c = _c.mul(1000).div(amtB.add(resB)).mul(resA);
 
     uint256 d = a.mul(c).mul(4);
-    uint256 e = AlpacaMath.sqrt(b.mul(b).add(d));
+    uint256 e = LatteSwapMath.sqrt(b.mul(b).add(d));
 
     uint256 numerator = e.sub(b);
     uint256 denominator = a.mul(2);
@@ -77,36 +79,40 @@ contract LatteSwapOptimalDeposit is ReentrancyGuardUpgradeable {
     return numerator.div(denominator);
   }
   
-  function optimalAddLiquidity(
+  function _optimalAddLiquidity(
       address tokenA,
       address tokenB,
       uint amtA,
       uint amtB,
-      uint minLPAmount,
+      uint minLiquidity,
       address to,
       uint deadline
-  ) external override nonReentrant {
+  ) internal returns (uint amountA, uint amountB, uint liquidity){
     // 1. Find out what token pair we are dealing with.
-    ILatteSwapPair lpToken = ILatteSwapPair(LatteSwapLibrary.pairFor(factory, tokenA, tokenB));
+    ILatteSwapPair lpToken = ILatteSwapPair(factory.getPair(tokenA, tokenB));
     // 2. Approve router to do their stuffs
     tokenA.safeApprove(address(router), uint256(-1));
     tokenB.safeApprove(address(router), uint256(-1));
     // 3. Compute the optimal amount of tokenA and tokenB to be converted.
     uint256 swapAmt;
     bool isReversed;
-    (swapAmt, isReversed) = calSwapAmount(
-      tokenA,
-      amtA,
-      tokenB,
-      amtB
-    );
+    {
+      (uint256 r0, uint256 r1, ) = lpToken.getReserves();
+      (uint256 tokenAReserve, uint256 tokenBReserve) = lpToken.token0() == tokenA ? (r0, r1) : (r1, r0);
+      (swapAmt, isReversed) = calSwapAmount(
+        amtA,
+        amtB,
+        tokenAReserve,
+        tokenBReserve
+      );
+    }
     // 4. Convert between tokenA and tokenB
     address[] memory path = new address[](2);
     (path[0], path[1]) = isReversed ? (tokenB, tokenA) : (tokenA, tokenB);
     // 5. Swap according to path
     if (swapAmt > 0) router.swapExactTokensForTokens(swapAmt, 0, path, address(this), deadline);
     // 6. Add liquidity and return all LP tokens to the sender.
-    (, , uint256 moreLPAmount) = router.addLiquidity(
+    (amountA, amountB, liquidity) = router.addLiquidity(
       tokenA,
       tokenB,
       tokenA.myBalance(),
@@ -116,7 +122,7 @@ contract LatteSwapOptimalDeposit is ReentrancyGuardUpgradeable {
       to,
       deadline
     );
-    require(moreLPAmount >= minLPAmount, "LatteSwapOptimalDeposit::execute:: insufficient LP tokens received");
+    require(liquidity >= minLiquidity, "LatteSwapOptimalDeposit::execute:: insufficient LP tokens received");
     require(
       lpToken.transfer(msg.sender, lpToken.balanceOf(address(this))),
       "LatteSwapOptimalDeposit::execute:: failed to transfer LP token to msg.sender"
@@ -124,6 +130,49 @@ contract LatteSwapOptimalDeposit is ReentrancyGuardUpgradeable {
     // 7. Reset approve to 0 for safety reason
     tokenA.safeApprove(address(router), 0);
     tokenB.safeApprove(address(router), 0);
+  }
+
+  function optimalAddLiquidity(
+      address tokenA,
+      address tokenB,
+      uint amtA,
+      uint amtB,
+      uint minLiquidity,
+      address to,
+      uint deadline
+  ) external nonReentrant returns (uint amountA, uint amountB, uint liquidity){
+    (amountA, amountB, liquidity) = _optimalAddLiquidity(
+      tokenA, 
+      tokenB, 
+      amtA, 
+      amtB, 
+      minLiquidity, 
+      to, 
+      deadline
+    );
+  }
+  
+  function optimalAddLiquidityBNB(
+    address token,
+    uint amount,
+    uint minLiquidity,
+    address to,
+    uint deadline
+  ) external payable nonReentrant returns (uint amountToken, uint amountBNB, uint liquidity){
+    // 1. Wrap BNB
+    if (msg.value != 0) {
+      IWBNB(WBNB).deposit{ value: msg.value }();
+    }
+    // 2. optimal add liquidity using WBNB
+    (amountToken, amountBNB, liquidity) = _optimalAddLiquidity(
+      token, 
+      WBNB, 
+      amount, 
+      msg.value, 
+      minLiquidity, 
+      to, 
+      deadline
+    );
   }
 
   receive() external payable {}
