225ab5e498a464e45af35af202b40a27c649e3d2
Earl Gray Frappuccino <elgrayfrappuccino.kub@gmail.com>
2021-09-24 16:13:58 +0700

    chore: format using prettier

--- contracts/swap/LatteSwapFactory.sol
+++ contracts/swap/LatteSwapFactory.sol
@@ -2,8 +2,8 @@
 
 pragma solidity 0.6.12;
 
-import './interfaces/ILatteSwapFactory.sol';
-import './LatteSwapPair.sol';
+import "./interfaces/ILatteSwapFactory.sol";
+import "./LatteSwapPair.sol";
 
 contract LatteSwapFactory is ILatteSwapFactory {
   address public override feeTo;
@@ -12,13 +12,13 @@ contract LatteSwapFactory is ILatteSwapFactory {
   mapping(address => mapping(address => address)) public override getPair;
   address[] public override allPairs;
 
-  event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+  event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
 
   constructor(address _feeToSetter) public {
     feeToSetter = _feeToSetter;
   }
 
-  function allPairsLength() external override view returns (uint) {
+  function allPairsLength() external view override returns (uint256) {
     return allPairs.length;
   }
 
@@ -27,10 +27,10 @@ contract LatteSwapFactory is ILatteSwapFactory {
   }
 
   function createPair(address tokenA, address tokenB) external override returns (address pair) {
-    require(tokenA != tokenB, 'LatteSwapFactory::createPair::IDENTICAL_ADDRESSES');
+    require(tokenA != tokenB, "LatteSwapFactory::createPair::IDENTICAL_ADDRESSES");
     (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-    require(token0 != address(0), 'LatteSwapFactory::createPair::ZERO_ADDRESS');
-    require(getPair[token0][token1] == address(0), 'LatteSwapFactory::createPair::PAIR_EXISTS'); // single check is sufficient
+    require(token0 != address(0), "LatteSwapFactory::createPair::ZERO_ADDRESS");
+    require(getPair[token0][token1] == address(0), "LatteSwapFactory::createPair::PAIR_EXISTS"); // single check is sufficient
     bytes memory bytecode = type(LatteSwapPair).creationCode;
     bytes32 salt = keccak256(abi.encodePacked(token0, token1));
     assembly {
@@ -44,12 +44,12 @@ contract LatteSwapFactory is ILatteSwapFactory {
   }
 
   function setFeeTo(address _feeTo) external override {
-    require(msg.sender == feeToSetter, 'LatteSwapFactory::setFeeTo::FORBIDDEN');
+    require(msg.sender == feeToSetter, "LatteSwapFactory::setFeeTo::FORBIDDEN");
     feeTo = _feeTo;
   }
 
   function setFeeToSetter(address _feeToSetter) external override {
-    require(msg.sender == feeToSetter, 'LatteSwapFactory::setFeeToSetter::FORBIDDEN');
+    require(msg.sender == feeToSetter, "LatteSwapFactory::setFeeToSetter::FORBIDDEN");
     feeToSetter = _feeToSetter;
   }
 }
