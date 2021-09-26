1cb80c0545a76cc3a88f63fff3d1651487f2905a
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-09-27 02:21:24 +0700

    feat: add a script for set a new bonus miltiplier to master barista using a timelock

---
+++ scripts/farm/timelock/timelock_set_bonus_multiplier_to_master_barista.ts
@@ -0,0 +1,47 @@
+import { ethers } from "hardhat";
+import { MasterBarista__factory } from "../../../compiled-typechain";
+import { FileService, TimelockService, ITimelockResponse, getConfig, withNetworkFile } from "../../../utils";
+
+async function main() {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  const config = getConfig();
+  const MULTIPLIER = "5";
+  const EXACT_ETA = "1632713400";
+
+  const timelockTransactions: Array<ITimelockResponse> = [];
+
+  console.log(">> Queue Transaction to set a new bonus multiplier through Timelock");
+  const masterBarista = MasterBarista__factory.connect(config.MasterBarista, (await ethers.getSigners())[0]);
+  const bonusEndBlock = await masterBarista.bonusEndBlock();
+  const bonusLockupBps = await masterBarista.bonusLockUpBps();
+
+  timelockTransactions.push(
+    await TimelockService.queueTransaction(
+      `setting a new bonus multiplier to ${MULTIPLIER} having the same endblock ${bonusEndBlock} and lock bps ${bonusLockupBps}`,
+      config.MasterBarista,
+      "0",
+      "setBonus(uint256,uint256,uint256)",
+      ["uint256", "uint256", "uint256"],
+      [MULTIPLIER, bonusEndBlock, bonusLockupBps],
+      EXACT_ETA
+    )
+  );
+  console.log("✅ Done");
+
+  await FileService.write("set-masterbarista-multiplier", timelockTransactions);
+}
+
+withNetworkFile(main)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
