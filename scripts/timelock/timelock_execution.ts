5f4fef6ef9ef86826fcec6a936b8f6951721275a
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-09-27 12:05:46 +0700

    chore: update network files and timelock execution file

--- scripts/timelock/timelock_execution.ts
+++ scripts/timelock/timelock_execution.ts
@@ -1,5 +1,5 @@
 import { ITimelockResponse, withNetworkFile } from "../../utils";
-import TimelockTransactions from "../results/develop/1631866417_set-staking-token-category-allowance.json";
+import TimelockTransactions from "../results/prod/1632554050_set-masterbarista-multiplier.json";
 import { FileService, TimelockService } from "../../utils";
 
 async function main() {
