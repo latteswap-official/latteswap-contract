abc80bd1848a84b76de35a908538b73a50191b22
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-09-29 17:59:20 +0700

    feat: use config rather than an address string

--- scripts/farm/add_dripbar_campaign_info.ts
+++ scripts/farm/add_dripbar_campaign_info.ts
@@ -24,20 +24,17 @@ async function main() {
   const config = getConfig();
   const CAMPAIGNS: IAddDripBarCampaignParamList = [
     {
-      NAME: "Mock Token #1",
-      STAKING_TOKEN: "0xcD855F77f939B626dBe3dAdB6bE7aab961fC8977",
-      REWARD_TOKEN: "0x117f7C7e629f2C097dC2897FfECcaFdf8c202117",
-      START_BLOCK: "11274650",
+      NAME: "Mock Token #2",
+      STAKING_TOKEN: config.BeanBag,
+      REWARD_TOKEN: "0x128EEB898Db09784262E346900EDA06815873065",
+      START_BLOCK: "11329540",
     },
   ];
 
   for (let i = 0; i < CAMPAIGNS.length; i++) {
     const campaign = CAMPAIGNS[i];
     console.log(`>> Execute Transaction to add ${campaign.NAME} to Dripbar`);
-    const dripbar = DripBar__factory.connect(
-      "0xe26067221a286EC1D3A914E192641513883A4b36",
-      (await ethers.getSigners())[0]
-    ) as DripBar;
+    const dripbar = DripBar__factory.connect(config.DripBar, (await ethers.getSigners())[0]) as DripBar;
     const tx = await dripbar.addCampaignInfo(campaign.STAKING_TOKEN, campaign.REWARD_TOKEN, campaign.START_BLOCK);
     await tx.wait();
     console.log(`>> returned tx hash: ${tx.hash}`);
--- scripts/farm/add_dripbar_reward_info.ts
+++ scripts/farm/add_dripbar_reward_info.ts
@@ -25,23 +25,22 @@ async function main() {
   const REWARDINFO: IAddDripBarRewardInfoParamList = [
     {
       PHASE_NAME: "WEEK_1",
-      CAMPAIGN_ID: "0",
-      ENDBLOCK: "11332250",
+      CAMPAIGN_ID: "1",
+      ENDBLOCK: "11502340",
       REWARD_PER_BLOCK: ethers.utils.parseEther("0.01").toString(),
     },
   ];
 
   for (let i = 0; i < REWARDINFO.length; i++) {
     const rewardInfo = REWARDINFO[i];
-    const dripbar = DripBar__factory.connect(
-      "0xe26067221a286EC1D3A914E192641513883A4b36",
-      (await ethers.getSigners())[0]
-    ) as DripBar;
+    const dripbar = DripBar__factory.connect(config.DripBar, (await ethers.getSigners())[0]) as DripBar;
 
     console.log(
       `>> Execute Transaction to add reward info for campaign#${rewardInfo.CAMPAIGN_ID} ${rewardInfo.PHASE_NAME}`
     );
-    const tx = await dripbar.addRewardInfo(rewardInfo.CAMPAIGN_ID, rewardInfo.ENDBLOCK, rewardInfo.REWARD_PER_BLOCK);
+    const tx = await dripbar.addRewardInfo(rewardInfo.CAMPAIGN_ID, rewardInfo.ENDBLOCK, rewardInfo.REWARD_PER_BLOCK, {
+      gasLimit: 10000000,
+    });
     await tx.wait();
     console.log(`>> returned tx hash: ${tx.hash}`);
     console.log("✅ Done");
