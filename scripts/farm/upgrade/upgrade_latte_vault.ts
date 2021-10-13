5d976b0b04cdcc800c33811888d47ea2c2cb1e85
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-10-13 12:13:14 +0700

    feat: add a script for upgrading a latte vault

---
+++ scripts/farm/upgrade/upgrade_latte_vault.ts
@@ -0,0 +1,34 @@
+import { ethers, network, upgrades } from "hardhat";
+import { LatteVault, LatteVault__factory } from "../../../typechain";
+import { getConfig, withNetworkFile } from "../../../utils";
+
+async function main() {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  const config = getConfig();
+
+  console.log(`>> Upgrading a LatteVault`);
+  const LatteVault = (await ethers.getContractFactory(
+    "LatteVault",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as LatteVault__factory;
+  const latteVault = (await upgrades.upgradeProxy(config.LatteVault, LatteVault)) as LatteVault;
+  await latteVault.deployed();
+  console.log(`✅ Done Upgrading a LatteVault`);
+}
+
+withNetworkFile(main)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
